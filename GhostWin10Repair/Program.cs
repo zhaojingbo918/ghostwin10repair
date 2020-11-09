a1100b705be38ee99bd715b20e647842053244f5
jingbo <jingbo@nolovr.com>
2020-11-09 19:31:25 +0800

    添加工程

---
+++ GhostWin10Repair/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostWin10Repair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("按任意键退出");
+            Console.ReadKey();
+        }
+    }
+}
